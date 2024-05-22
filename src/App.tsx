ea456b98584a184bcf58026505168efa6cb7c48c
Nico Lee <new.nikolee2007@gmail.com>
2024-05-22 19:41:19 +0700

    Pass Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import ListGroup from "./component/ListGroup";
+// import ListGroup from "./component/ListGroup";
+import  Alert  from "./component/Alert";
 
 function App(){
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  
-  const handleSelectItem = (item: string)=>{
-    console.log(item);
-  }
+  return (
+    <div className="alert alert-primary">
+      <Alert>
+    Hello <span>World</span>
+        </Alert>
+    </div>
+  );
 
-  return <div>
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-  </div>;
+  // let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+  
+  // const handleSelectItem = (item: string)=>{
+  //   console.log(item);
+  // }
+
+  // return <div>
+  //   <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+  // </div>;
 }
 
 export default App;
